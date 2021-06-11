beefb155bfec8d417e83c1b4f6c6ba8ef68774e8
Mikita Kupryianau <nikita.kupriyan0v@yahoo.com>
2021-06-11 16:43:55 +0300

    feat: update subtask1

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,25 +1,16 @@
 package subtask1
 
-import java.text.ParseException
 import java.text.SimpleDateFormat
-import java.util.*
 
 class DateFormatter {
 
+    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
         return if (month.toInt() in 1..12) {
-            val simpleDateFormat =
-                SimpleDateFormat("dd/MM/yyyy")
-            var myDate: Date? = null
-            try {
-                myDate = simpleDateFormat.parse("$day/$month/$year")
-            } catch (e: ParseException) {
-                e.printStackTrace()
-            }
-            simpleDateFormat.applyPattern("dd MMMM, EEEE")
-            assert(myDate != null)
-            if (simpleDateFormat.format(myDate).substring(0, 2) == day) {
-                simpleDateFormat.format(myDate)
+            val string: String =
+                SimpleDateFormat("d MMMM, EEEE").format(SimpleDateFormat("dd/MM/yyyy").parse("$day/$month/$year"))
+            if (string.substring(0, 2).contains(day) ) {
+                string
             } else {
                 "Такого дня не существует"
             }
