8b7f3a68a691340c2be1793d416b26282cc19df7
Mikita Kupryianau <nikita.kupriyan0v@yahoo.com>
2021-06-11 16:56:26 +0300

    feat: update 2.0 subtask1

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,6 +1,7 @@
 package subtask1
 
 import java.text.SimpleDateFormat
+import java.util.*
 
 class DateFormatter {
 
@@ -8,7 +9,7 @@ class DateFormatter {
     fun toTextDay(day: String, month: String, year: String): String {
         return if (month.toInt() in 1..12) {
             val string: String =
-                SimpleDateFormat("d MMMM, EEEE").format(SimpleDateFormat("dd/MM/yyyy").parse("$day/$month/$year"))
+                SimpleDateFormat("d MMMM, EEEE", Locale("ru")).format(SimpleDateFormat("dd/MM/yyyy").parse("$day/$month/$year"))
             if (string.substring(0, 2).contains(day) ) {
                 string
             } else {
